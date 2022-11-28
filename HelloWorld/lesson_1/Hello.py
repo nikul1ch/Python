a3dfee96f3cb81e5aa4234ac67ca6de548b7bbce
nikul1ch <nikulin@netkam.ru>
2022-11-28 15:05:13 +0300

    first commit

---
+++ HelloWorld/lesson_1/Hello.py
@@ -0,0 +1,16 @@
+print('hello world')
+
+a = 123
+b = 1.23
+print(a)
+print(b)
+print(type(a))
+print(type(b))
+print(b, ' * 100  = ', a)
+
+f = True  # bool
+print(type(f))
+list = ['1', '2', '3', '4']
+print(list)
+
+n = input()
